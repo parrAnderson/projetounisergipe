0f93d1002aa5769797ca7c126904d8b0a4b7b740
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2020-03-16 19:34:20 -0300

    Passando footer para boostrap

--- src/modules/layouts/Footer.js
+++ src/modules/layouts/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+// import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import styled from "styled-components";
 import logo from "../../assets/img/logo-min.png"
 import smartUFS from "../../assets/img/smartufs.png"
@@ -9,39 +9,57 @@ import DCOMP from "../../assets/img/dcomp.png"
 
 const FooterPage = () => {
   return (
-    <MDBFooter style={{ backgroundColor: "#004694"}}color="blue" className="font-small pt-4 mt-4">
-      
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow>
-          <MDBCol md="6">
-          <Image src={logo} alt="UFS" /><h5 style={{ color: 'white'}} className="title">Projeto Fala UFS</h5>
-            <p style={{ color: 'white'}}>
-            </p>
-            
-          </MDBCol>
-          <MDBCol md="6">
-            <h5 style={{ color: 'white', marginTop: 0}} className="title">Colaboradores</h5>
-            <Image src={smartUFS} alt="smartUFS" />
-            <Image src={DAU} alt="DAU" />
-            <Image src={DCOMP} alt="DCOMP" />
-            
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer style={{ color: 'white'}} fluid>
-          &copy; {new Date().getFullYear()} <a style={{ color: 'white'}} href="/"> Universidade Federal de Sergipe </a>
-        </MDBContainer>
+    <div>
+   
+   <div className="row fixed-bottom bg-primary">
+     <div className="col">
+     <div className="row justify-content-center ">
+      <div className="col-10">
+        <div className="row text-white justify-content-center">
+        <div className="col-4 col-sm-4 col-md-2 col-lg-1 align-self-center">
+          <Image className="img-fluid" src={logo} alt="UFS" />
+        </div>
+        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 text-center align-self-center">
+          <h5>Projeto Fala UFS</h5>          
+        </div>
+        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+          <div className="row">
+            <div className="col text-center">
+              <h6>Colaboradores</h6> 
+            </div>
+          </div>
+          <div className="row">       
+            <div className="col ">
+              <Image className="img-fluid" src={smartUFS} alt="smartUFS" />        
+            </div>
+            <div className="col">
+              <Image className="img-fluid" src={DAU} alt="DAU"/>          
+            </div>
+            <div className="col">
+              <Image className="img-fluid" src={DCOMP} alt="DCOMP" />
+            </div>
+          </div>
+        </div>
+        <div className="col text-center align-self-center">
+          <span> &copy; {new Date().getFullYear()} Universidade Federal de Sergipe</span>        
+        </div>
+        </div>  
       </div>
-    </MDBFooter>
+    </div>
+     </div>
+    
+   </div>
+   
+    
+    </div>
   );
 }
 
 const Image = styled.img`
-  height: 50px;
-  width: 130px;
-  margin-top: 0px;
-  margin-left: 50px;
+  // height: 50px;
+  // width: 130px;
+  // margin-top: 0px;
+  // margin-left: 50px;
 `;
 
 export default FooterPage;
